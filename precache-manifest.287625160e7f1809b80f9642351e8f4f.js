4e296cbfd38fa6b25b9ddd0487a88c8357225428
HarcourtHamsa <hamsaharcourt@gmail.com>
2019-10-06 18:32:55 +0000

    Updates

---
+++ precache-manifest.287625160e7f1809b80f9642351e8f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c4b1c0626e416ee6f075c41b00a43f",
+    "url": "/BuyPrime/index.html"
+  },
+  {
+    "revision": "8eae1757d203173b7815",
+    "url": "/BuyPrime/static/css/main.3e7bc87d.chunk.css"
+  },
+  {
+    "revision": "7e572c7612a18697d7eb",
+    "url": "/BuyPrime/static/js/2.5373bfcd.chunk.js"
+  },
+  {
+    "revision": "8eae1757d203173b7815",
+    "url": "/BuyPrime/static/js/main.005b8bf2.chunk.js"
+  },
+  {
+    "revision": "df05809c335f6c6f3ff7",
+    "url": "/BuyPrime/static/js/runtime-main.55882129.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/BuyPrime/static/media/bg.3644ee70.jpg"
+  }
+]);
